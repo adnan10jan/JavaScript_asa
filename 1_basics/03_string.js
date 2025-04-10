f2de1b79a76fd7fa835531cfba562ce94926ec80
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-04-10 19:10:49 +0000

    string uses

---
+++ 1_basics/03_string.js
@@ -0,0 +1,16 @@
+const name = "Adnan"
+const repoCount = 50
+
+console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('adnan')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+
