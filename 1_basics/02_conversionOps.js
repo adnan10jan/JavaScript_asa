13535f01ad6e822569fb780218597e921ad15322
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-04-10 14:42:01 +0000

    updated

--- 1_basics/02_conversionOps.js
+++ 1_basics/02_conversionOps.js
@@ -1,12 +1,20 @@
 let score = "33asa"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
+
+
+//*******Ops************ */
+
+let value = 4
+let negValue = -value
+
+console.log(negValue);
 
 
 
