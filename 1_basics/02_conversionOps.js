57ca29a0322d6ed8ddf4ea057a0be7b2a46aa3e4
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-04-10 14:17:12 +0000

    conversion of data types

---
+++ 1_basics/02_conversionOps.js
@@ -0,0 +1,13 @@
+let score = "33asa"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+
+
