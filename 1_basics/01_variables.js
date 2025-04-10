554abf4ad475c7d6b5b2cf21e05e76a9c590a1b4
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-04-10 14:07:10 +0000

    config

---
+++ 1_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 1234
+let accountEmail = "asa@gmail.com"
+var accountPass = "1231"
+accountCity = "Bangalore"
+
+
+/*
+var is not using nowadays because->
+issue in block scope and functional scope
+*/
+
+console.table([accountID, accountEmail, accountPass, accountCity]);
