ee998e348bf709a0a0ded524a139d403f7c9034f
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-04-14 10:13:21 +0000

    math and num

---
+++ 1_basics/04_math_and_num.js
@@ -0,0 +1,22 @@
+const number = 400
+//console.log(number);
+
+const newNumber = new Number(100)
+//console.log(newNumber);
+
+//console.log(newNumber.toString().length);
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString('en-IN'));  // it is used for defining numbers in indian standard
+
+//++++++++++++++++ MATHS ++++++++++++++++++ //
+
+console.log(Math);
+console.log(Math.abs(-4));      // abs used for absolute value which converts negative to positive val.
+
+console.log(Math.random());     // random method is ued for selecting num from 0 to 1.
+
+
+
+
+
