4a56b10aa0dea00670a6b584e8c52fb482ded287
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-04-14 11:44:08 +0000

    dates

---
+++ 1_basics/05_dates.js
@@ -0,0 +1,8 @@
+let myDate = new Date()
+//console.log(myDate.toString());
+
+let myCreatedDate = new Date("01-14-2025")
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myCreatedDate.getTime());
