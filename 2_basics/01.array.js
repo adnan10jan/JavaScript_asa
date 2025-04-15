5cbc55ceae309a25618ea3ea5b0171574584f270
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-04-15 10:26:11 +0000

    array uses

---
+++ 2_basics/01.array.js
@@ -0,0 +1,33 @@
+// array
+
+const myArr = [1,2,3,4,5]
+//console.log(myArr);
+
+const arr2 = new Array(1,2,3,4)
+//console.log(arr2);
+
+
+// array methods //
+
+//myArr.push(6)
+//onsole.log(myArr);
+
+//myArr.unshift(10)
+//console.log(myArr);
+
+//myArr.shift()
+//onsole.log(myArr);
+
+
+// slice and splice //
+
+const mynr = myArr.slice(1,3)
+
+console.log(mynr);
+console.log("B", myArr);
+
+
+const mynr2 = myArr.splice(1, 3)
+console.log("c", myArr);
+console.log(mynr2);
+
