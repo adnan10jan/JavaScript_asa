6c288e18e0daa7eee09030acfe081486f6c73d89
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-04-16 10:38:23 +0000

    function use of rest

--- 3_basics/01_function.js
+++ 3_basics/01_function.js
@@ -12,7 +12,15 @@ function sayMyName() {
 // const result = addTwoNumber(3, 6)
 // console.log("Result", result );
 
-function loginUser(username) {
-    return `${username} just logged in`
+// function loginUser(username) {
+//     return `${username} just logged in`
+// }
+// console.log( loginUser("adnan"))
+
+
+function calculateCardPrice(...num1) {
+    return num1
 }
-loginUser("adnan")
+
+console.log(calculateCardPrice(200, 300, 500, 600, "asa"));
+
