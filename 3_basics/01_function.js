479ccffa68bc8601e7460f1293ff9b0f02463652
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-04-16 10:09:31 +0000

    updated

--- 3_basics/01_function.js
+++ 3_basics/01_function.js
@@ -3,4 +3,16 @@ function sayMyName() {
     console.log("asa");
     console.log("saif");
 }
-sayMyName()
+//sayMyName()
+
+// function addTwoNumber(num1, num2) {
+//     return num1 + num2
+// }
+
+// const result = addTwoNumber(3, 6)
+// console.log("Result", result );
+
+function loginUser(username) {
+    return `${username} just logged in`
+}
+loginUser("adnan")
