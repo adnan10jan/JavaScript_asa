9c792229ce2aa9186cac35f3318139ebcebb39d4
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-04-17 17:01:16 +0000

    immediate function (IIFE0

---
+++ 3_basics/04_iife.js
@@ -0,0 +1,9 @@
+// immediately invoked function expression (IIFE) 
+
+(function goku() { 
+    console.log(`become utra instinct`);        // this func. used for prvent any pollution from global scope variable/declaration
+})();
+
+((name) => {
+    console.log(`become master UI ${name}`);        // wheneves we use IIFE we have to end function by applying `;` 
+})("kakarot")
