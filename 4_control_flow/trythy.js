3676bde6040adffaa8c448f37e26895c698fb8ff
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-04-18 16:56:08 +0000

    truth and false statement

---
+++ 4_control_flow/trythy.js
@@ -0,0 +1,24 @@
+const userEmail = []
+
+if (userEmail.length === 0) {
+    console.log("empty array");
+}
+
+const emptyObj = {}
+
+if(Object.keys(Object).length === 0) {
+    console.log('empty object');
+}
+
+
+// Nullish coalescing operator (??) numm or undefined
+
+let val1;
+//val1 = 5 ?? 10
+val1 = null ?? 10
+
+
+
+
+
+console.log(val1);
