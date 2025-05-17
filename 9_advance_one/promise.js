75edfda10ced3fbbf1e208c167aa9a799d877cfc
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-05-17 17:32:51 +0000

    promise in js

---
+++ 9_advance_one/promise.js
@@ -0,0 +1,60 @@
+const promiseOne = new Promise(function(resolve, reject){
+    // do async task
+    // do db calls, cryptography, network calls
+    setTimeout(function() {
+        console.log("async task is cmpleted");
+        resolve()
+    }, 1000)
+})
+
+promiseOne.then(function() {
+    console.log('promise consumed');
+})
+
+new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        console.log('async tas 2 completed');
+        resolve()
+    }, 1000)
+
+}).then(function() {
+    console.log('prmised 2 resolved')
+})
+
+const promiseThree = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        resolve({username: "asa", email: "asa@aaa.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user) {
+    console.log(user); 
+})
+
+const promiseFour = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        let error = false
+        if(!error) {
+            resolve({username: "adnan", pass: "aaa"})
+        } else {
+            reject('Error: something went wrong')
+        }
+    }, 1000)
+})
+
+promiseFour.then((user) => {
+    console.log(user);
+    return user.username;
+}).then((username) => {
+    console.log(username);
+}).catch((err) => {
+    console.log(err);
+}).finally(() => console.log('promise is either resolved or rejected'))
+
+
+
+
+
+
+
+
