98348c94af28828b82015b9322e6508164a9e4c7
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-05-19 16:45:36 +0000

    fetch uses

--- 9_advance_one/promise.js
+++ 9_advance_one/promise.js
@@ -51,9 +51,49 @@ promiseFour.then((user) => {
     console.log(err);
 }).finally(() => console.log('promise is either resolved or rejected'))
 
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function() {
+        let error = true
+        if(!error) {
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('Error: js went wrong')
+        }
+    }, 1000)
+})
 
+// async function consumePromise() {
+//     const response = await promiseFive
+//     console.log(response);
 
+async function consumePromise() {
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error)
+    }
+}
+consumePromise()
 
+// async function getAllUser() {
+//     const response = await fetch('https://api.github.com/users/adnan10jan')
+//     //console.log(response);
+//     const data = await response.json()
+//     console.log(data);
+// }
+// getAllUser()
+
+fetch('https://api.github.com/users/adnan10jan')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => {
+    console.log(error)
+})
 
 
 
