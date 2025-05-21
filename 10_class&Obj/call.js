a6f24e96b370b6c23fbcfbf136fa84276be22b80
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-05-21 15:03:38 +0000

    using of call and this

---
+++ 10_class&Obj/call.js
@@ -0,0 +1,15 @@
+function setUserName(username) {
+    this.username = username
+    console.log('called');
+    
+}
+
+function createUser(username, email, pass) {
+    setUserName.call(username)
+    this.email = email
+    this.pass = pass
+}
+
+const asa = new createUser("adnan", "asa@122", "abcd" )
+
+console.log(asa);
