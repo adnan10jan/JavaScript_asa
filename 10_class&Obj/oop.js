60b7167951c343f818ff4d251e6d025bdb8b925a
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-05-21 15:02:52 +0000

    oops in deatils

--- 10_class&Obj/oop.js
+++ 10_class&Obj/oop.js
@@ -16,9 +16,14 @@ function User(username, isLoggedIn, loginCount) {
     this.isLoggedIn = isLoggedIn,
     this.loginCount = loginCount
     //return this
+
+    this.greeting = function() {
+        console.log(`Welcome ${this.username}`);
+    }
+    return this
 }
 
 let data = new User("adnan", 13, 11)
 let data2 = new User("asa", 11, 23)
-console.log(data)
-console.log(data2)
+console.log(data.constructor)
+//console.log(data2)
