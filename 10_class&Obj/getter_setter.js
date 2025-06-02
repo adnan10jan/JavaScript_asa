a4e2f5b40e5461147bccf83f5e4c3dba595c11b2
Adnan <103173461+adnan10jan@users.noreply.github.com>
2025-06-02 14:04:17 +0000

    getter setter uses

---
+++ 10_class&Obj/getter_setter.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(email, password) {
+        this.email = email;
+        this.password = password
+    }
+
+    get email() {
+        return this._email.toUpperCase()
+    }
+    set email(value) {
+        this._email = value
+    }
+
+    get password() {
+        return `${this._password}goku`
+    }
+    
+    set password(value) {
+        this._password = value
+    }
+}
+
+const adnan = new User("asa@123", "saif")
+console.log(adnan.email);
+console.log(adnan.password);
+
